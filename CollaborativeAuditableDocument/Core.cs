2fbe5fe48ec7bd2a12abbec210914fa1c2695e65
SimonFlensted <simon_flensted@live.dk>
2018-11-28 09:46:27 +0100

    Core added

---
+++ CollaborativeAuditableDocument/Core.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollaborativeAuditableDocument
+{
+    public sealed class Core
+    {
+        private string user { get; set; }
+        private static Core instance = null;
+
+        private Core()
+        {
+        }
+
+        public static Core Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Core();
+                }
+                return instance;
+            }
+        }
+
+        public bool login(string user)
+        {
+            return true;
+        }
+    }
+}
