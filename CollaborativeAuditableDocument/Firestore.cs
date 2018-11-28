6ddd65709386a600e96cb72f07598e4758197ac4
Steffen Hansen <stef.hansen@live.dk>
2018-11-28 10:29:19 +0100

    Added Section class and work on Firestore class

--- CollaborativeAuditableDocument/Firestore.cs
+++ CollaborativeAuditableDocument/Firestore.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 using Google.Cloud.Firestore;
+using CollaborativeAuditableDocument.FirestoreEntities;
 
 namespace CollaborativeAuditableDocument
 {
@@ -15,6 +17,50 @@ namespace CollaborativeAuditableDocument
                 instance = new Firestore();
             }
             return instance;
+        }
+
+        public FirestoreDb db;
+
+        public Firestore() {
+            db = FirestoreDb.Create();
+        }
+
+        public async Task<Section[]> GetSections() {
+            CollectionReference docRef = db.Collection("sections");
+            QuerySnapshot qs = await docRef.GetSnapshotAsync();
+            List<Section> sections = new List<Section>();
+            foreach(DocumentSnapshot ds in qs.Documents) {
+                if (ds.Exists) {
+                    Section s = ds.ConvertTo<Section>();
+                    sections.Add(s);
+                }
+            }
+            return sections.ToArray();
+        }
+
+        public async Task<string> SaveSection(Section section) {
+            DocumentReference docRef = await db.Collection("sections").AddAsync(section);
+            return docRef.Id;
+        }
+
+        public async Task<string> addNewSection(string username, string title, string text, int order) {
+            CollectionReference colRef = db.Collection("sections");
+            string[] approvedBy = { username };
+            Section section = new Section {
+                Title = title,
+                Text = text,
+                Order = order,
+                CreatedAt = DateTime.Now,
+                CreatedBy = username,
+                ApprovedBy = approvedBy
+            };
+            DocumentReference docRef = await colRef.AddAsync(section);
+            return docRef.Id;
+        }
+
+        public async Task approveSection(string username, string sectionId) {
+            DocumentReference docRef = db.Collection("sections").Document(sectionId);
+
         }
     }
 }
---
+++ CollaborativeAuditableDocument/FirestoreEntities/Section.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Cloud.Firestore;
+
+namespace CollaborativeAuditableDocument.FirestoreEntities {
+    [FirestoreData]
+    public class Section {
+        [FirestoreProperty]
+        public string Title { get; set; }
+        [FirestoreProperty]
+        public string Text { get; set; }
+        [FirestoreProperty]
+        public int Order { get; set; }
+        [FirestoreProperty]
+        public string CreatedBy { get; set; }
+        [FirestoreProperty]
+        public DateTime CreatedAt { get; set; }
+        [FirestoreProperty]
+        public string[] ApprovedBy { get; set; }
+        [FirestoreProperty]
+        public DateTime ApprovedAt { get; set; }
+    }
+}
