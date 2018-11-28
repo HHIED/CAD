b898a9c617bfdd175514472346645511501e8708
Steffen Hansen <stef.hansen@live.dk>
2018-11-28 12:57:06 +0100

    Added environment variables n' shit

--- CollaborativeAuditableDocument/Program.cs
+++ CollaborativeAuditableDocument/Program.cs
@@ -14,6 +14,7 @@ namespace CollaborativeAuditableDocument
         [STAThread]
         static void Main()
         {
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "keyfile.json");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login());
